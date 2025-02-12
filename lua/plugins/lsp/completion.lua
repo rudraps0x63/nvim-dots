82268b0028cab4f0bdd121ec805fce533ec01098
Rudra Pratap Singh <rudrasingh901@yahoo.com>
2025-02-13 00:02:35 +0530

    blink.cmp: Reconfigure keymap

--- lua/plugins/lsp/completion.lua
+++ lua/plugins/lsp/completion.lua
@@ -1,21 +1,29 @@
+local Util = require('util.util')
+
 return {
   {
     'saghen/blink.cmp', version = '0.11.0',
     dependencies = 'rafamadriz/friendly-snippets',
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'none',
+        ['K'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+        ['T'] = { 'hide', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+      },
       sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
         cmdline = {} -- Don't autocomplete for vim commands
       },
       completion = {
-        menu = { border = 'rounded' },
+        menu = { border = Util.PREFERRED_BORDER_STYLE },
         documentation = {
-          window = { border = 'single' }
+          window = { border = Util.PREFERRED_BORDER_STYLE }
         },
       },
       signature = {
-        window = { border = 'single' }
+        window = { border = Util.PREFERRED_BORDER_STYLE }
       },
     },
   },
